0d2565691f7957825d0c803b27afa390f48a7fd9
Jimmy M Andersson <dev@applyn.se>
2019-03-03 17:43:12 +0100

    Add QuickSort

---
+++ QuickSort/QuickSort.playground/Contents.swift
@@ -0,0 +1,63 @@
+extension Array where Element: Comparable {
+  
+  public mutating func quickSort() {
+    var slice = self[0..<self.endIndex]
+    quickSort(&slice)
+  }
+  
+  private func quickSort(_ array: inout ArraySlice<Element>) {
+    if array.count < 2 {
+      return
+    }
+    
+    sortPivot(in: &array)
+    let pivot = partition(&array)
+    
+    var firstSlice = array[array.startIndex..<pivot]
+    var secondSlice = array[pivot + 1..<array.endIndex]
+    
+    quickSort(&firstSlice)
+    quickSort(&secondSlice)
+  }
+  
+  private func partition(_ array: inout ArraySlice<Element>) -> ArraySlice<Element>.Index {
+    let midPoint = (array.startIndex + array.endIndex) / 2
+    array.swapAt(midPoint, array.startIndex)
+    let pivot = array[array.startIndex]
+    
+    var lower = array.startIndex
+    var upper = array.endIndex - 1
+    
+    repeat {
+      while lower < array.endIndex - 1 && array[lower] <= pivot {
+        lower += 1
+      }
+      while array[upper] > pivot {
+        upper -= 1
+      }
+      
+      if lower < upper {
+        array.swapAt(lower, upper)
+      }
+    } while lower < upper
+    
+    array.swapAt(array.startIndex, upper)
+    return upper
+  }
+  
+  private func sortPivot(in array: inout ArraySlice<Element>) {
+    let startPoint = array.startIndex
+    let midPoint = (array.startIndex + array.endIndex) / 2
+    let endPoint = array.endIndex - 1
+    
+    if array[startPoint] > array[midPoint] {
+      array.swapAt(startPoint, midPoint)
+    }
+    if array[midPoint] > array[endPoint] {
+      array.swapAt(midPoint, endPoint)
+    }
+    if array[startPoint] > array[midPoint] {
+      array.swapAt(startPoint, midPoint)
+    }
+  }
+}
---
+++ QuickSort/README.md
@@ -0,0 +1,3 @@
+# QuickSort
+
+Example code for the Medium article "Sorting Algorithms: Implementing Quick Sort Using Swift"
